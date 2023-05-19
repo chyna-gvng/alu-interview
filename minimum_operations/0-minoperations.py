b73224ec4596c0e65ca87d47ee0b1f64a7f41aa9
Walter <w.angoye@alustudent.com>
2023-05-19 23:45:01 +0300

    minimum_operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,27 +6,26 @@
 
 
 def minOperations(n):
-    """
-        Calculates the fewest number of operations needed,
-        to result in exactly n H characters in the file.
+    """ Calculates the fewest number of operations needed
+        to result in exactly n H characters in the file
 
         Args:
-            n: The number of H characters to reach.
+            n: repetitions of H
+
         Returns:
-            The fewest number of operations needed.
-        Raises:
-            ValueError: If n is impossible to achieve.
+            number of operations (Copy & Paste) to reach n Hs
     """
+    operations = 0
+    summation = 1
+    carrier = 0
 
-    if n <= 0:
-        raise ValueError("n must be positive")
+    while summation < n:
+        if n % summation == 0:  # Copy when summation is a multiple of n
+            carrier = summation
+            summation *= 2
+            operations += 1
+        else:
+            summation += carrier
+        operations += 1  # Always paste
 
-    # Initialize the DP table.
-    dp = [float("inf")] * (n + 1)
-
-    # Fill in the DP table.
-    dp[1] = 0
-    for i in range(2, n + 1):
-        dp[i] = min(dp[i - 1] + 1, dp[i // 2] + 1, dp[i // 3] + 1)
-
-    return dp[n]
+    return operations
