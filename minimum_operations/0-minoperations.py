6d10f777b1d44daa0c09a657a4d817704a945863
Walter <w.angoye@alustudent.com>
2023-05-19 23:28:57 +0300

    minimum_operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Calculates the fewest number of operations needed to result in exactly n H characters in the file.
+"""
+
 def minOperations(n):
   """
   Calculates the fewest number of operations needed to result in exactly n H characters in the file.
