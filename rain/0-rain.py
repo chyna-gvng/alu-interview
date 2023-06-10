c16cad7bcd5cf26f3cf4403b812ea2010d9091d8
Walter <w.angoye@alustudent.com>
2023-06-10 17:42:15 +0300

    Updated 0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -4,34 +4,28 @@
 """
 
 def rain(walls):
-  """
-  Calculates the amount of water retained after it rains.
+    """
+    Parameters:
+        walls: ist of non-negative integers
 
-  Args:
-    walls: A list of non-negative integers representing the heights of walls with unit width 1.
+    Returns:
+        int: total amount of rainwater collected
+    """
+    if walls is None:
+        return 0
 
-  Returns:
-    The amount of water retained, in square units.
-  """
+    walls_length = len(walls)
+    max_water = 0
 
-  # Initialize the amount of water retained to 0.
-  water_retained = 0
+    if walls == 0 or walls_length == 1:
+        return 0
 
-  # Iterate through the walls, from left to right.
-  for i in range(len(walls)):
-
-    # If the current wall is higher than the previous wall, then there is water that can be retained.
-    if walls[i] > walls[i - 1]:
-
-      # Find the index of the first wall that is lower than the current wall.
-      index = i
-      while index < len(walls) or walls[index] >= walls[i]:
-        index += 1
-
-      # Check if the index is less than the length of the list.
-      if index < len(walls):
-        # Calculate the amount of water that can be retained.
-        water_retained += (index - i) * walls[i]
-
-  # Return the amount of water retained.
-  return water_retained
+    for i in range(1, walls_length - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, walls_length):
+            right = max(right, walls[j])
+        max_water = max_water + (min(left, right) - walls[i])
+    return max_water
