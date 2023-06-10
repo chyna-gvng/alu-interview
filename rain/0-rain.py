e9dc2c160c466f4b1728fcab67bfe8cb6bc659fa
Walter <w.angoye@alustudent.com>
2023-06-10 17:36:36 +0300

    Updated 0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+    0-rain
+"""
+def rain(walls):
+  """
+  Calculates the amount of water retained after it rains.
+
+  Args:
+    walls: A list of non-negative integers representing the heights of walls with unit width 1.
+
+  Returns:
+    The amount of water retained, in square units.
+  """
+
+  # Initialize the amount of water retained to 0.
+  water_retained = 0
+
+  # Iterate through the walls, from left to right.
+  for i in range(len(walls)):
+
+    # If the current wall is higher than the previous wall, then there is water that can be retained.
+    if walls[i] > walls[i - 1]:
+
+      # Find the index of the first wall that is lower than the current wall.
+      index = i
+      while walls[index] >= walls[i]:
+        index += 1
+
+      # Calculate the amount of water that can be retained.
+      water_retained += (index - i) * walls[i]
+
+  # Return the amount of water retained.
+  return water_retained
